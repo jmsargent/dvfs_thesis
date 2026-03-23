899de70169d1ea18d41813b64fec32a91a94c2ff
sargent <jmsargent94@gmail.com>
2026-03-23 15:44:25 +0100

    build now gives others permission to run mustard binaries

---
+++ mustard/build_mustard.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Use your local modern CMake binary
+LOCAL_CMAKE="/data/users/sargent/mustard/cmake-3.28.1-linux-x86_64/bin/cmake"
+
+apptainer exec --nv /data/users/sargent/dvfs_thesis/containers/container.sif bash -c "
+    # 1. Clean
+    rm -rf build && mkdir -p build
+
+    # 2. Patch (ensure we are in root)
+    sed -i 's|nvshmem::nvshmem|-lnvshmem_host -lnvshmem_device|g' CMakeLists.txt
+
+    cd build
+
+    # 3. Configure using Absolute Internal Paths
+    # Note: I'm using the paths defined in your .def file
+    $LOCAL_CMAKE .. \
+        -DCMAKE_CXX_COMPILER=g++ \
+        -DCMAKE_CUDA_COMPILER=/opt/nvidia/hpc_sdk/Linux_x86_64/24.9/cuda/12.6/bin/nvcc \
+        -DCMAKE_CUDA_ARCHITECTURES=89 \
+        -DMUSTARD_CUDA_ARCHITECTURES=89 \
+        -DCMAKE_EXE_LINKER_FLAGS=\"-L/opt/nvidia/hpc_sdk/Linux_x86_64/24.9/comm_libs/12.6/nvshmem/lib\" \
+        -DCMAKE_CUDA_FLAGS=\"-I/opt/nvidia/hpc_sdk/Linux_x86_64/24.9/comm_libs/12.6/nvshmem/include -L/opt/nvidia/hpc_sdk/Linux_x86_64/24.9/comm_libs/12.6/nvshmem/lib -lnvshmem_host -lnvshmem_device -lcuda\"
+
+    # 4. Build
+    make -j\$(nproc)
+"
+
+# Provide 'search' access to the directory chain
+chmod o+x /data/users/sargent
+chmod o+x /data/users/sargent/dvfs_thesis
+chmod o+x /data/users/sargent/dvfs_thesis/mustard
+chmod o+x /data/users/sargent/dvfs_thesis/mustard/build
+
+# Grant Read and Execute to the specific binaries
+chmod o+rx /data/users/sargent/dvfs_thesis/mustard/build/lu_mustard
+chmod o+rx /data/users/sargent/dvfs_thesis/mustard/build/cg_mustard
+chmod o+rx /data/users/sargent/dvfs_thesis/mustard/build/cholesky_mustard
+chmod o+rx /data/users/sargent/dvfs_thesis/mustard/build/p_lu_mustard
+chmod o+rx /data/users/sargent/dvfs_thesis/mustard/build/pingpong_mustard
