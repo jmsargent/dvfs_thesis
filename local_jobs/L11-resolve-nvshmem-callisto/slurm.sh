d41b3d6acb8d32b331d3de8e7221797b0aced895
Jonathan Sargent <jmsargent94@gmail.com>
2026-04-04 17:33:54 +0200

    diagnose callisto, resolve nvshmem

---
+++ local_jobs/L11-resolve-nvshmem-callisto/slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:L4:1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=local_jobs/L10-resolve-nvshmem-callisto/result_%j.out
+#SBATCH --nodelist=callisto
+
+CONTAINER_PATH="/data/users/sargent/dvfs_thesis/containers/container.sif"
+
+apptainer exec --nv "$CONTAINER_PATH" bash -c "
+    export LD_LIBRARY_PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/24.9/comm_libs/12.6/nvshmem/lib:\$LD_LIBRARY_PATH
+    ldd /data/users/chjing/dvfs_thesis/mustard/build/cholesky_mustard | grep nvshmem
+"
